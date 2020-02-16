cdddd08decb6a73fa376e9cf92ebe2949e46040d
yuyabanjou <yuyabanjou@gmail.com>
2020-02-16 12:28:56 +0000

    itinerary_spotの追加、削除、更新 ok

--- app/controllers/itinerary_spots_controller.rb
+++ app/controllers/itinerary_spots_controller.rb
@@ -9,24 +9,61 @@ class ItinerarySpotsController < ApplicationController
 
 	def create
 		@itinerary = Itinerary.find(params[:itinerary_id])
-		@itinerary_spot = ItinerarySpot.new(itinerary_spot_params)
-
+		@itinerary_spot = @itinerary.itinerary_spots.new(itinerary_spot_params)
 		respond_to do |format|
-			if @itinerary_spot.save
+			if @itinerary_spot.save!
 				format.html { redirect_to @itinerary_spot }
-				format.json { render :show, status: :created, location: @itinerary_spot }
+				format.json { render :new, status: :created, location: @itinerary_spot }
 				format.js { @status = "success"}
 			else
-        format.html { render :new }
-        format.json { render json: @itinerary_spot.errors, status: :unprocessable_entity }
-        format.js { @status = "fail" }
-      end
-    end
+				format.html { render :new }
+				format.json { render json: @itinerary_spot.errors, status: :unprocessable_entity }
+				format.js { @status = "fail" }
+			end
+		end
+	end
+
+	def destroy
+		@itinerary = Itinerary.find(params[:itinerary_id])
+		itinerary_spot = @itinerary.itinerary_spots.find(params[:id])
+		itinerary_spot.destroy
+		redirect_to edit_traveller_itinerary_path(id: @itinerary.id)
+		# 非同期削除の途中
+		# respond_to do |format|
+		# 	if itinerary_spot.destroy
+		# 		format.html { redirect_to @itinerary_spot }
+		# 		format.json { render :new, status: :created, location: @itinerary_spot }
+		# 		format.js { @status = "success"}
+		# 	else
+
+		# 	end
+	end
+
+	def edit
+		@itinerary = Itinerary.find(params[:itinerary_id])
+		@favorite_spots = current_traveller.my_favorite_spots
+		@itinerary_spot = @itinerary.itinerary_spots.find(params[:id])
+	end
+
+	def update
+		@itinerary = Itinerary.find(params[:itinerary_id])
+		@itinerary_spot = @itinerary.itinerary_spots.find(params[:id])
+		respond_to do |format|
+			if @itinerary_spot.update(itinerary_spot_params)
+				format.html { redirect_to @itinerary_spot }
+				format.json { render :update, status: :created, location: @itinerary_spot }
+				format.js { @status = "success"}
+			else
+				format.html { render :update }
+				format.json { render json: @itinerary_spot.errors, status: :unprocessable_entity }
+				format.js { @status = "fail" }
+			end
+		end
 	end
 
 	private
 	def itinerary_spot_params
-		params.require(:itinerary_spot).permit(:itinerary_id, :spot_id, :date, :itinerary_spot_memo, :reference_url, :moving_genre, :moving_memo, :fare, :arrival_plan_time, :start_plan_time)
+		params.require(:itinerary_spot).permit(:spot_id, :date, :itinerary_spot_memo, :reference_url, :moving_genre, :moving_memo, :fare, :arrival_plan_time, :start_plan_time)
 	end
 
 end
