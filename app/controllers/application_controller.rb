d9cf587d2e32c0aaed3111afe0386ae422c3204e
yuyabanjou <yuyabanjou@gmail.com>
2020-03-02 04:35:12 +0000

    deviseエラー日本語化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
 
 	def after_sign_in_path_for(resource)
-	  root_path
+	  if current_traveller
+	  	flash[:notice] = "ログインに成功しました。"
+	  	spots_path
+	  else
+	  	flash[:notice] = "新規登録完了しました。次にニックネームを入力してください。"
+	  	edit_traveller_path
+	  end
 	end
 
 	def after_sign_out_path_for(resource)
