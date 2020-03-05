bc239d3b78a1f92fd20265ab3bab81b184b456bc
yuyabanjou <yuyabanjou@gmail.com>
2020-03-05 07:32:47 +0000

    登録画面微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
 class ApplicationController < ActionController::Base
 
 	def after_sign_in_path_for(resource)
-	  if current_traveller
-	  	flash[:notice] = "ログインに成功しました。"
-	  	spots_path
-	  else
-	  	flash[:notice] = "新規登録完了しました。次にニックネームを入力してください。"
-	  	edit_traveller_path
-	  end
+	  flash[:notice] = "ログインしました。"
+	  spots_path
 	end
 
 	def after_sign_out_path_for(resource)
