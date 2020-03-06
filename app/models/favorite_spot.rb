f72d57151637b0f726375768ff133e1a6f651eed
yuyabanjou <yuyabanjou@gmail.com>
2020-03-06 07:47:09 +0000

    validation途中

--- app/models/favorite_spot.rb
+++ app/models/favorite_spot.rb
@@ -2,6 +2,6 @@ class FavoriteSpot < ApplicationRecord
 
 	# お気に入り機能
 	belongs_to :traveller
-    belongs_to :spot
+  belongs_to :spot
 
 end
--- app/models/itinerary_spot.rb
+++ app/models/itinerary_spot.rb
@@ -5,4 +5,14 @@ class ItinerarySpot < ApplicationRecord
 
 	enum moving_genre: {徒歩: 0, 電車: 1, バス: 2, 船: 3, その他: 4}
 
+	# バリデーションを書きたかったが保留
+	# validates :arrival_plan_time_after_start_plan_time
+
+	# private
+	# 	def arrival_plan_time_after_start_plan_time
+	# 		unless self.arrival_plan_time > self.start_plan_time
+	# 			errors.add(:arrival_plan_time, ": 到着日時は出発日時より後に設定してください")
+	# 		end
+	# 	end
+
 end
